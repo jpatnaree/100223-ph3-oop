2c0a6c191125735fda8c5a6988b22f230419ab08
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-14 11:50:17 -0500

    lecture

--- classes/human.py
+++ classes/human.py
@@ -1,2 +1,35 @@
 class Human:
-    pass
+    def __init__(self, first_name, last_name, age = 0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self._age = age # or self._age = 0 and property on top canbe only {age}
+
+        print("Finish generating 1 human")
+
+    def __repr__(self):
+        return f"specie: Human\n name: {self.last_name}, {self.first_name}\n age {self._age} year-old"
+    
+    def say_full_name(self):
+        print(f'Human full-name: {self.first_name} {self.last_name}')
+
+    def happy_birthday(self):
+        self._age += 1
+        print(self._age)
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, new_age):
+        raise TypeError('Quit lying about your age!')
+
+
+luke = Human("Luke", "Skywalker")
+print(luke)
+luke.happy_birthday()
+
+luke.happy_birthday()
+
+# luke.age = 4
+print(luke)
--- classes/robot.py
+++ classes/robot.py
@@ -1,2 +1,45 @@
 class Robot:
-    pass
+
+    def __init__(self, name, friend, color, height): #Chett called it magic method
+        print("Building A Robot")
+        self.name = name
+        self.friend = friend
+        self.color = color
+        self._height = height # private variable or attribute
+        print("Building Complete")
+        #should not be returning any String => innit create something and return it for ya!
+
+    def __repr__(self):
+        return f"Robot(name = {self.name} friend={self.friend} color={self.color} height={self._height})"
+    
+    def walk(self):
+        print('Robot is walking! Boop! Beep!')
+        print(self) # self is instant
+
+    #Reader/Getter
+    @property # => to modify the height
+    def height(self):
+        return self._height # if call .height this is what is called
+    
+    #Writer/Setter
+    @height.setter # to set new height
+    def height(self, new_height):
+        if type(new_height) == int or type(new_height) == float: 
+        # if isinstance(new_height, int) or isinstance(new_height, float): # this one works too
+            self._height = new_height # .height = "???" is calling this one
+        else:
+            raise TypeError("Height must be an integer")
+        
+    # @height.setter # this one is for read-only, data cannot be set again
+    # def height(self, new_height):
+    #         raise TypeError("You may not change this property") 
+
+c3po = Robot("C3PO", "R2D2", "White", 6.3) # Does not have to be in order, switchble
+c3po.walk()
+
+c3po.planet_of_origin = "Tatooine"
+c3po.height = 7.4
+
+print(c3po.planet_of_origin)
+print(c3po.height)
+
